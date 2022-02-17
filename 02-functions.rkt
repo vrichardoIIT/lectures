2d9afdd520cb9464fe1d895872026f00dd534082
Michael Lee <lee@soi2.org>
2022-02-17 11:28:03 -0600

    Adding some more examples

--- 02-functions.rkt
+++ 02-functions.rkt
@@ -18,7 +18,7 @@
              (println z)
              (* x (+ y z))))
 
-(define (f3 x) (+ x 1))
+(define (f3 x) (add1 x))
 
 (define (f4 x y z)
   (println x)
@@ -51,6 +51,16 @@
 (define (f6 . rest)
   (length rest))
 
+;; lambda expressions also support rest arguments
+(define f7
+  (lambda args ; `args` is a list of all the arguments
+    (length args)))
+
+(define f8
+  (lambda (x . rest)
+    (println x)
+    (println rest)))
+
 
 #|-----------------------------------------------------------------------------
 ;; Some more special forms
@@ -61,6 +71,7 @@
 - `cond`: multi-way conditional
 - `case`: dispatch
 - `match`: pattern match
+- `set!`: mutation!
 -----------------------------------------------------------------------------|#
 
 (define (say-hi-1 name)
@@ -134,3 +145,11 @@
     [(list _ _ _ "Jane") (println "Hi, Jane in 4th place")]
     [(or 'Jane '(Jane)) (format "Hi, Jane in hiding")]
     [_ (println "Stranger, Danger!")]))
+
+
+(define (say-hi-7 name)
+  (let ([greetings '("Hello" "Hola" "你好")]
+        [index 0])
+    (lambda ()
+      (println (format "~a, ~a" (list-ref greetings index) name))
+      (set! index (remainder (add1 index) (length greetings))))))
