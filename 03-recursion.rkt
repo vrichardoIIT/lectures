920bcacdc1c1a5aa7be78832701d81cfbdb4e355
Michael Lee <lee@soi2.org>
2022-09-07 14:58:28 -0500

    Adding examples

--- 03-recursion.rkt
+++ 03-recursion.rkt
@@ -4,51 +4,72 @@
 
 
 #|-----------------------------------------------------------------------------
-;; Recursion
+;; Recursion (and Iteration)
 -----------------------------------------------------------------------------|#
+
+(define (println-times datum n)
+  (when (> n 0)
+    (println datum)
+    (println-times datum (sub1 n))))
+
+
+;; integer summation
+(define (sum-to n)
+  (if (= n 0)
+      0
+      (+ n (sum-to (sub1 n)))))
+; (trace sum-to)
+
+;; This version uses an accumulator to avoid doing work after the recursive
+;; call returns. This lets Racket performs tail-call optimization (TCO).
+(define (sum-to-acc n acc)
+  (if (= n 0)
+      acc
+      (sum-to-acc (sub1 n) (+ acc n))))
+; (trace sum-to-acc)
+
+;; Can use `letrec` to define local recursive functions.
+(define (sum-to-acc-2 n)
+  (letrec ([sum (lambda (i acc) ; can use trace-lambda for tracing
+                  (if (= i 0)
+                      acc
+                      (sum (sub1 i) (+ acc i))))])
+    (sum n 0)))
+
+;; Alternatively, can use `define` to define a local function
+(define (sum-to-acc-3 n)
+  (define (sum i acc)
+    (if (= i 0)
+        acc
+        (sum (sub1 i) (+ acc i))))
+  ; (trace sum)
+  (sum n 0))
+
+;; Can also use alternative form of `let`.
+(define (sum-to-acc-4 n)
+  (let sum ([i n] ; can use trace-let for tracing
+            [acc 0])
+    (if (= i 0)
+        acc
+        (sum (sub1 i) (+ acc i)))))
+
 
 ;; Fibonacci series: 0 1 1 2 3 5 8 13 21 34 55 ...
 (define (fib n)
   (if (< n 2)
       n
       (+ (fib (- n 1)) (fib (- n 2)))))
-#; (trace fib)
+; (trace fib)
 
-(define (sum-to n)
-  (if (= n 0)
-      0
-      (+ n (sum-to (- n 1)))))
-#; (trace sum-to)
+;; using an accumulator
+(define (fib-tail n)
+  (let rec ([f0 0]
+            [f1 1]
+            [m 0])
+    (if (= m n)
+        f0
+        (rec f1 (+ f0 f1) (add1 m)))))
 
-;; This version uses an accumulator to avoid doing work after the recursive
-;; call returns. This lets Racket performs tail-call optimization (TCO).
-(define (sum-to-acc n sum)
-  (if (= n 0)
-      sum
-      (sum-to-acc (- n 1) (+ sum n))))
-#; (trace sum-to-acc)
-
-;; Can use `letrec` to define local recursive functions.
-(define (sum-to-acc-2 n)
-  (letrec ([rec (lambda (i sum) ; can use trace-lambda for tracing
-                  (if (= i 0)
-                     sum
-                     (rec (- i 1) (+ sum i))))])
-    (rec n 0)))
-
-;; Can also use alternative form of `let`.
-(define (sum-to-acc-3 n)
-  (let rec ([i n] ; can use trace-let for tracing
-            [acc 0])
-    (if (= i 0)
-        acc
-        (rec (- i 1) (+ acc i)))))
-
-;; Recursion *is* iteration.
-(define (loop i)
-  (when (> i 0)
-    (println "hello")
-    (loop (- i 1))))
 
 
 #|-----------------------------------------------------------------------------
