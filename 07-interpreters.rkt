4e4b198f479821f8ae7ea71b2d999058c2728d98
Michael Lee <lee@soi2.org>
2022-09-30 14:38:58 -0500

    Adding more details on parsing

--- 07-interpreters.rkt
+++ 07-interpreters.rkt
@@ -27,6 +27,15 @@
 
 (define d1 (doohickey "thingamajig" "thinging" 199.99 "time travel"))
 
+;; we can also match against structs
+(define (which-widget? w)
+  (match w
+    [(widget "wrench" _ _) "It's a wrench!"]
+    [(doohickey "thingamajig" _ _ _) "It's a thingamajig!"]
+    [(? widget?) "It's some sort of widget"]
+    [_ "I don't know what this is"]))
+
+
 
 #|-----------------------------------------------------------------------------
 ;; Our language
@@ -37,38 +46,67 @@ first iteration will support integer literals, the binary arithmetic operations
 -----------------------------------------------------------------------------|#
 
 ;; Some test cases
-(define p1 '(+ 1 2))
+(define p1 "(+ 1 2)")
 
-(define p2 '(* 2 (+ 3 4)))
+(define p2 "(* 2 (+ 3 4))")
 
-(define p3 '(+ x 1))
+(define p3 "(+ x 1)")
 
-(define p4 '(* w (+ x y)))
+(define p4 "(* w (+ x y))")
 
-(define p5 '(let ([x 10])
-              (+ x 1)))
+(define p5 "(let ([x 10])
+              (+ x 1))")
 
-(define p6 '(let ([x 10]
+(define p6 "(let ([x 10]
                   [y 20])
-              (+ x y)))
+              (+ x y))")
 
-(define p7 '(let ([x 10])
+(define p7 "(let ([x 10])
               (let ([y 20])
                 (+ x (let ([w 30])
-                       (* w y))))))
+                       (* w y)))))")
 
-
-#|-----------------------------------------------------------------------------
-;; Scanner
-
-Review: What is scanning?
------------------------------------------------------------------------------|#
 
 
 #|-----------------------------------------------------------------------------
 ;; Parser
 
 Review: What is parsing?
+
+- Input string (source language) => Syntax object
+
+- A syntax object contains information about the structure of the code. Often,
+  we use a *tree* as an underlying representation.
+
+  - E.g., the code "(let ([x 10]) (+ x 1))"
+          
+          may be parsed to the syntax tree:
+
+                    let
+                   /   \
+                  x     +
+                 /     / \
+                10    x   1
+
+  - Racket sexps are a programmatic way of representing syntax trees!
+
+- Since our syntax mirrors Racket's, we may rely on Racket's reader to parse
+  our input to produce an initial syntax tree.
+-----------------------------------------------------------------------------|#
+
+;;; using Racket's reader
+#; (read)
+
+
+
+#|-----------------------------------------------------------------------------
+;; Parser (continued)
+
+- Our syntax tree doesn't currently contain much information besides tokens
+  pulled directly from the input string.
+
+- Next, we will recursively descend through the syntax tree, "decorating" its
+  nodes with information that can help us expand and evaluate it.
 -----------------------------------------------------------------------------|#
 
 ;;; Some types for decorating our syntax tree
@@ -94,7 +132,7 @@ Review: What is parsing?
 #|-----------------------------------------------------------------------------
 ;; Interpreter
 
-Syntax tree => Evaluation
+- The interpreter's job is the take the (decorated) syntax tree and evalute it!
 -----------------------------------------------------------------------------|#
 
 ;; Interpreter
