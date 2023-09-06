9c6dd3f8b828e473e0766ca7b5a57feda1cf9583
Michael Lee <lee@soi2.org>
2023-09-06 13:42:14 -0500

    Switching to trace-* definitions

--- 03-recursion.rkt
+++ 03-recursion.rkt
@@ -18,57 +18,62 @@
   (if (= n 0)
       0
       (+ n (sum-to (sub1 n)))))
-; (trace sum-to)
+
+(trace sum-to)
+
 
 ;; This version uses an accumulator to avoid doing work after the recursive
 ;; call returns. This lets Racket performs tail-call optimization (TCO).
-(define (sum-to-acc n acc)
+(trace-define (sum-to-acc n acc)
   (if (= n 0)
       acc
-      (sum-to-acc (sub1 n) (+ acc n))))
-; (trace sum-to-acc)
+      (sum-to-acc (sub1 n) (+ n acc))))
+
 
 ;; Can use `letrec` to define local recursive functions.
 (define (sum-to-acc-2 n)
-  (letrec ([sum (lambda (i acc) ; can use trace-lambda for tracing
+  (letrec ([sum (trace-lambda (i acc)
                   (if (= i 0)
                       acc
                       (sum (sub1 i) (+ acc i))))])
     (sum n 0)))
 
+
 ;; Alternatively, can use `define` to define a local function
 (define (sum-to-acc-3 n)
-  (define (sum i acc)
+  (trace-define (sum i acc)
     (if (= i 0)
         acc
         (sum (sub1 i) (+ acc i))))
-  ; (trace sum)
   (sum n 0))
+      
+
 
 ;; Can also use alternative form of `let`.
 (define (sum-to-acc-4 n)
-  (let sum ([i n] ; can use trace-let for tracing
-            [acc 0])
+  (trace-let sum ([i n]
+                  [acc 0])
     (if (= i 0)
         acc
         (sum (sub1 i) (+ acc i)))))
 
 
 ;; Fibonacci series: 0 1 1 2 3 5 8 13 21 34 55 ...
-(define (fib n)
-  (if (< n 2)
+(trace-define (fib n)
+  (if (<= n 1)
       n
-      (+ (fib (- n 1)) (fib (- n 2)))))
-; (trace fib)
+      (+ (fib (- n 1))
+         (fib (- n 2)))))
+
 
 ;; using an accumulator
 (define (fib-tail n)
-  (let rec ([f0 0]
-            [f1 1]
-            [m 0])
-    (if (= m n)
+  (trace-let rec ([f0 0]
+                  [f1 1]
+                  [i 0])
+    (if (= i n)
         f0
-        (rec f1 (+ f0 f1) (add1 m)))))
+        (rec f1 (+ f0 f1) (add1 i)))))
 
 
 
@@ -78,26 +83,43 @@
 - recursion over lists is an example of "structural recursion"
 -----------------------------------------------------------------------------|#
 
-(define (length lst)
+(trace-define (length lst)
   (if (empty? lst)
       0
       (+ 1 (length (rest lst)))))
 
-(define (repeat n x)
+
+(trace-define (repeat n x)
   (if (= n 0)
       '()
       (cons x (repeat (- n 1) x))))
 
-(define (reverse lst)
-  (let rec ([lst lst]
-            [acc '()])
+
+(trace-define (concat l1 l2)
+  (cond [(empty? l1) l2]
+        [(empty? l2) l1]
+        [else (cons (car l1)
+                    (concat (cdr l1) l2))]))
+
+
+(trace-define (reverse lst)
+  (if (empty? lst)
+      lst
+      (concat (reverse (cdr lst))
+              (list (car lst)))))
+
+
+(define (reverse-tail lst)
+  (trace-let rec ([lst lst]
+                   [acc '()])
     (if (empty? lst)
         acc
         (rec (rest lst) (cons (first lst) acc)))))
 
+
 (define (range n)
-  (let rec ([i 0]
-            [acc '()])
+  (trace-let rec ([i 0]
+                  [acc '()])
     (if (= i n)
-        (reverse acc)
+        (reverse-tail acc)
         (rec (+ i 1) (cons i acc)))))
