6fb052521a18650c7f1412d593bd25a51ce66385
Vincent Richardo <vrichardo@hawk.iit.edu>
2023-10-02 19:21:12 -0500

    6

--- 06-lang.rkt
+++ 06-lang.rkt
@@ -89,15 +89,16 @@ So what *really* happens when we load a source file into a Racket interpreter?
                       ,@(map make-form src-lines)
                       players)))) ;,@ is like the python unpacking  ,@src-lines
 
-(define players (make-hash))
 
+(provide update players)
+
+(define players (make-hash))
 
 (define (update name val)
   (hash-set! players name (+ val (hash-ref players name 0))))
 
 
-(provide update players)
-
+;GO TO COMPLETED NOTES!!!!
 
 #|
 (make-hash) creates hashtables (mutable)
